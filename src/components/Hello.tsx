d6160fb994ac7b8e29d0c5ef227cdb9dc83afa12
TAKAHASHI TOSHIYA <ttakahashi@nzigen.com>
2023-08-26 11:22:19 +0900

    first_commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,15 @@
+const Hello = () => {
+    const onclick = () => {
+        alert('Hello')
+    }
+    const text = 'Hello, React'
+
+    return (
+        <div onClick={onclick}>
+            {text}
+        </div>
+    )
+}
+
+export default Hello
+
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Parent } from "./components/UseCallbackSample";
+
+ReactDOM.render(
+
+        <Parent />,
+
+    document.getElementById('root')
+);
+
